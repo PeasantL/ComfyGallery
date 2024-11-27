5cc033a24233f6d7405778ce039d7376c672564f
PeasantL <samuel.tan99@rocketmail.com>
2024-11-27 11:09:31 +0800

    Syle formatting, Made grid cubic, overwrote style to contain image.

--- frontend/src/components/ImageCard.jsx
+++ frontend/src/components/ImageCard.jsx
@@ -1,28 +1,35 @@
 import Card from '@mui/material/Card'
 import CardMedia from '@mui/material/CardMedia'
-import CardContent from '@mui/material/CardContent'
 import Typography from '@mui/material/Typography'
 import PropTypes from 'prop-types'
 import '../styles/ImageCard.css'
 
 const ImageCard = ({ image, onClick }) => (
   <Card className="image-card" onClick={onClick}>
-    <CardMedia
-      component="img"
-      className="image-card-media"
-      image={image.thumbnail} // Thumbnail URL passed directly
-      alt="Thumbnail"
-    />
-    <CardContent className="image-card-content">
-      <Typography variant="subtitle2" gutterBottom>
-        {image.title} {/* Use title directly if available */}
-      </Typography>
-    </CardContent>
+    <div className="image-card-overlay-container">
+      <CardMedia
+        component="img"
+        className="image-card-media"
+        image={image.thumbnail}
+        alt="Thumbnail"
+        style={{
+          objectFit: 'contain', //Can't increase spec. generated css
+        }}
+      />
+      <div className="image-card-overlay">
+        <Typography variant="subtitle2" className="image-card-text">
+          {image.title}
+        </Typography>
+      </div>
+    </div>
   </Card>
 )
 
 ImageCard.propTypes = {
-  image: PropTypes.object.isRequired, // Expect the `thumbnail` URL
+  image: PropTypes.shape({
+    thumbnail: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  }).isRequired,
   onClick: PropTypes.func.isRequired,
 }
 
